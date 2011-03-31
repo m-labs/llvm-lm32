7e076ec6d99fccb40c23a0afe1da0fd22a2aabbf
JP Bonn <jpbonn@meta-systems.com>
2011-03-31 12:09:23 -0700

    Added TargetInfo directory and files.

---
+++ lib/Target/Mico32/TargetInfo/Mico32TargetInfo.cpp
@@ -0,0 +1,19 @@
+//===-- Mico32TargetInfo.cpp - Mico32  Target Implementation ---------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Mico32 .h"
+#include "llvm/Module.h"
+#include "llvm/Target/TargetRegistry.h"
+using namespace llvm;
+
+Target llvm::TheMico32Target;
+
+extern "C" void LLVMInitializeMico32TargetInfo() {
+  RegisterTarget<Triple::mico32> X(TheMico32Target, "mico32", "Mico32");
+}
